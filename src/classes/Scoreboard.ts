5c68200c7dc2b00693749a15d64cffc5ccfcb368
Jordi Cantó Gálvez <developsuricata@gmail.com>
2024-09-15 22:58:57 +0200

    Implement basic methods of Scoreboard class to pass tests

--- src/classes/Scoreboard.ts
+++ src/classes/Scoreboard.ts
@@ -4,19 +4,52 @@ export default class Scoreboard {
     private activeGames: ActiveGame[] = [];
 
     startGame(matchup: Matchup, startTime?: number): void {
-        // Method implementation here
+        const activeGame: ActiveGame = {
+            ...matchup,
+            homeScore: 0,
+            awayScore: 0,
+            startedAt: startTime ?? Date.now(),
+        };
+
+        this.activeGames.push(activeGame);
     }
 
     updateScore(gameResult: GameResult): void {
-        // Method implementation here
+        const gameToUpdate = this.activeGames.find(
+            game =>
+                game.homeTeam === gameResult.homeTeam &&
+                game.awayTeam === gameResult.awayTeam
+        );
+
+        if (gameToUpdate) {
+            gameToUpdate.homeScore = gameResult.homeScore;
+            gameToUpdate.awayScore = gameResult.awayScore;
+        }
     }
 
     finishGame(matchup: Matchup): void {
-        // Method implementation here
+        this.activeGames = this.activeGames.filter(
+            game =>
+                game.homeTeam !== matchup.homeTeam &&
+                game.awayTeam !== matchup.awayTeam
+        );
     }
 
     getSummary(): string[] {
-        // Method implementation here
-        return [];
+        const sortedActiveGames = this.activeGames.sort((a, b) => {
+            const totalScoreA = a.homeScore + a.awayScore;
+            const totalScoreB = b.homeScore + b.awayScore;
+
+            if (totalScoreA === totalScoreB) {
+                return a.startedAt - b.startedAt;
+            }
+
+            return totalScoreB - totalScoreA;
+        });
+
+        return sortedActiveGames.map(
+            game =>
+                `${game.homeTeam} ${game.homeScore} - ${game.awayTeam} ${game.awayScore}`
+        );
     }
 }
